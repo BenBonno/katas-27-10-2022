40ff74f595c0dea602f92d545beccfd6dbaa04b3
ArthurHtbk <arthur.heurtebise@gmail.com>
2022-10-27 00:10:10 +0100

    first commit

---
+++ 1-pigLatin.js
@@ -0,0 +1,52 @@
+// Nos jeunes amis d'outre-atlantique ont
+// une sorte de code secret. En effet,
+// lorsqu'ils ne veulent pas être compris
+// par les adultes, ils se mettent à parler
+// une forme de dialecte qui, pour les
+// francophones, s'apparenteraient à
+// l'enfant bâtard du verlan et du louchebem.
+// Ce code s'appelle le pig latin.
+
+// L'idée est de prendre la première lettre
+// d'un mot, de la basculer à la fin, et d'y
+// ajouter les lettres "ay" en guise de
+// conclusion.
+
+// Votre mission, que vous l'acceptiez ou non,
+// est d'écrire une fonction qui prendra en
+// paramètre un chaîne de caractères en anglais,
+// et qui retournera cette même chaîne de
+// caractères traduite en pig latin, afin
+// d'aider les jeunes à ne pas être compris
+// des adultes dans leur vie.
+
+// Bien entendu, en guise d'aide aux parents,
+// vous écrirez la fonction inverse, qui prend
+// en paramètre une chaîne de caractère en pig
+// latin et retourne son équivalent en anglais.
+
+// La chaîne de caractères ne contient ni
+// majuscules, ni ponctuations, et, parce que
+// je ne suis pas un monstre, je vous laisse
+// la possibilité d'utiliser des méthodes de
+// manipulation de chaînes de caractères.
+
+// Happy coding!
+
+const toPigLatin = (str) => {
+  // La magie s'opère ici!
+};
+
+const toEnglish = (str) => {
+  // La magie s'opère ici!
+};
+
+console.log(toPigLatin("my students are a bunch of freaking gems"));
+// Le résultat attendu :
+// ymay tudentssay reaay aay unchbay foay reakingfay emsgay
+
+console.log(
+  toEnglish("ymay tudentssay reaay aay unchbay foay reakingfay emsgay")
+);
+// Le résultat attendu :
+// my students are a bunch of freaking gems
---
+++ 2-inAFrame.js
@@ -0,0 +1,29 @@
+// Pour ce kata vous allez devoir écrire
+// une fonction qui prend en paramètre
+// une chaîne de caractères et qui, à
+// la manière d'un beau cadeau, affiche
+// dans la console cette même chaîne de
+// caractères entourée d'un cadre fait
+// d'étoiles (comme ceci : *). Chaque
+// mot s'affiche sur une ligne distincte,
+// et les dimensions du cadre ne doivent
+// pas être affectées par la longueur
+// des différents mots...
+
+// Happy coding!
+
+const inAFrame = (str) => {
+  // La magie s'opère ici!
+};
+
+inAFrame("Hakuna matata mais quelle phrase magnifique");
+// Le résultat attendu :
+//
+// **************
+// * Hakuna     *
+// * matata     *
+// * mais       *
+// * quelle     *
+// * phrase     *
+// * magnifique *
+// **************
